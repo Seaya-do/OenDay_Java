46207603370e6b4a3a98d02af340bb19bef512cf
Seaya-do <67862904+Seaya-do@users.noreply.github.com>
2023-04-05 00:14:16 +0900

    04/05

--- src/Java_One/DoWhile.java
+++ src/Java_One/DoWhile.java
@@ -1,8 +1,25 @@
 package Java_One;
 
+import java.util.Scanner;
+
 public class DoWhile {
+    public static void main(String[] args) {
+        int num=0;
+        do {
+            System.out.println("I Like JAVA " + num);
+            num++;
+        } while(num < 5);
 
+        //하나의 양의 정수를 입력받아서 각 자리수를 떼어냄
+        Scanner sc = new Scanner(System.in);
+        System.out.println("숫자를 입력 후 Enter : ");
 
-
-
+        int n = sc.nextInt();
+        while (n > 0){
+            int m = n % 10;
+            System.out.println(m);
+            n /= 10;
+        }
+        sc.close();
+    }
 }
